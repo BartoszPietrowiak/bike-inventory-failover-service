cf231691192123e992472cc2425b158c1b5d8bb7
bpietrowiak <bpietrowiak@consdata.pl>
2023-06-10 11:09:18 +0200

    Initial commit for failover service

---
+++ src/test/java/com/bike/failover/FailoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bike.failover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FailoverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
